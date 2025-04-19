d61ffb5a2217219d9f4e969af4e378d93346f177
thiaguinho700 <thiagobreakgo@gmail.com>
2025-04-19 16:54:06 -0300

    'att'

--- starwars/src/App.vue
+++ starwars/src/App.vue
@@ -5,10 +5,10 @@
       <li v-for="person in people" :key="person.name">
         {{ person.name }}
         <!-- <button @click="addToFavorites(person)">Add to Favorites</button> -->
-        <input @click="addToFavorites(person)" va type="checkbox">
+        <input @click="addToFavorites(person)" va type="checkbox" />
       </li>
     </ul>
-    
+
     <h2>Favorites</h2>
     <ul v-if="favorites.length">
       <li v-for="fav in favorites" :key="fav.name">{{ fav.name }}</li>
@@ -17,30 +17,33 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
       people: [],
-      favorites: []
+      favorites: [],
     };
   },
+  
   methods: {
-    fetchData() {
-      axios.get("https://swapi.dev/api/people")
-        .then(response => {
+    async fetchData() {
+      await axios
+        .get("http://swapi.co/api/people")
+        .then((response) => {
+          console.log("Dados recebidos:", response.data);
           this.people = response.data.results;
         })
-        .catch(error => {
-          console.error("Error fetching data:", error);
+        .catch((error) => {
+          console.error("Erro ao buscar dados:", error.message, error);
         });
     },
     addToFavorites(person) {
-      if (!this.favorites.some(fav => fav.name === person.name)) {
+      if (!this.favorites.some((fav) => fav.name === person.name)) {
         this.favorites.push(person);
       }
-    }
-  }
+    },
+  },
 };
 </script>
